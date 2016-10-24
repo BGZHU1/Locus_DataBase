ca0491d33324e1642412f0d349beb61bdd94d385
bijiezhu <bijizhu@yahoo.com>
2016-10-24 18:47:27 -0400

    DataBase

---
+++ testFile.sql
@@ -0,0 +1,5 @@
+INSERT INTO password ( idPassWord, UserName,PassWord)
+                       VALUES
+                       ( 2, "bijie", "1234" );
+                       
+select * from password 
